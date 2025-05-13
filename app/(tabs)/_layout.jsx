f40d7b3be735c2d3df6b2006dc74c325adc751b6
arslan433 <arslan.reactdev@gmail.com>
2025-05-13 20:50:04 +0500

    second commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const TabLayout = () => {
+  return (
+    <Tabs screenOptions={{ headerShown: true }}>
+      <Tabs.Screen name='home' options={{title:'Home'}}/>
+      <Tabs.Screen name='history' options={{title:'History'}}/>
+      <Tabs.Screen name='profile' options={{title:'Profile'}}/>
+        
+    </Tabs>
+  )
+}
+
+export default TabLayout
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { RelativePathString, router, useRouter } from "expo-router";
+import { StatusBar, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter()
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl text-red-700">Edit app/index.tsx to edit this screen.</Text>
+      className="flex-1 items-center justify-center">
+      <Text className="text-xl text-black-700">Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity onPress={() => router.push("/home" as RelativePathString)} >
+        <Text>Change Route</Text>
+      </TouchableOpacity>
     </View>
   );
 }
