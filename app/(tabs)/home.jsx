a8f691552d192a5867704ab8f3132d35b8cce837
arslan433 <arslan.reactdev@gmail.com>
2025-05-20 00:57:22 +0500

    7th commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, ScrollView } from 'react-native'
-import React from 'react'
+// import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 const logo = require("../../assets/images/dinetimelogo.png")
 
@@ -8,16 +8,17 @@ const home = () => {
 
     <SafeAreaView style={{ backgroundColor: '#2b2b2b' }}>
       <View className='items-center flex'>
-        <View className='bg-slate-500  rounded-lg shadow-lg justify-between items-center flex flex-row p-2'>
+        <View className='bg-slate-500 w-11/12 rounded-lg shadow-lg justify-between items-center flex flex-row p-2'>
           <View className='flex flex-row'>
-            <Text className='text-6xl text-white' >Wellcome to</Text>
-          <Text className='text-2xl text-white'>HIIIIIIIII</Text>
+            <Text className='text-white text-lg' >Welcome to</Text>
             <Image source={logo} style={{ height: 25, width: 100 }} resizeMode='cover' />
           </View>
         </View>
       </View>
+      <ScrollView>
+       
+      </ScrollView>
     </SafeAreaView>
   )
 }
-
 export default home
